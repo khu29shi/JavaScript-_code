ee6e9064283d5d234b3bf6df1309cfe25884b335
Khushboo <khushbookarsh1112@gmail.com>
2024-05-17 13:47:31 +0530

    added new program

---
+++ Callbacks/triplate_sum.js
@@ -0,0 +1,16 @@
+let arr = [2,5,6,7,4]
+let target = 13;
+
+let threeSum = function(nums,target){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            for(let k=j+1; k<arr.length; k++){
+                if(arr[i] + arr[j] + arr[k] === target){
+                    return[i,j,k]
+                }
+            }
+        }
+    }
+    return[];
+}
+console.log(threeSum(arr,target));
