f7387a237f769ab37f4717be60ec9a3e5f65e2d1
Khushboo <khushbookarsh1112@gmail.com>
2024-05-17 13:33:20 +0530

    changes in code

--- Callbacks/two_no._sum.js
+++ Callbacks/two_no._sum.js
@@ -1,14 +1,15 @@
-let arr = [2,11,8,15,5,4]
+let arr = [2,11,7,15,5,4]
 let target = 9;
+let count = 0;
 
 let twoSum = function(nums,target){
     for(let i=0; i<arr.length; i++){
         for(let j=i+1; j<arr.length; j++){
             if(arr[i] + arr[j] === target){
-                return[i,j];
+                count++
             }
         }
     }
-    return[];
+    return count;
 }
 console.log(twoSum(arr,target));
